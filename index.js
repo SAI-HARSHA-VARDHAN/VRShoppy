a149dc7355a47ca11725b7264b3ab0fac1fea230
SAI-HARSHA-VARDHAN <harsha111hero@gmail.com>
2020-10-31 13:17:01 +0530

    fixes

--- index.js
+++ index.js
@@ -348,6 +348,6 @@ app.get('/cat', (req, res)=>{
     //   });
 });
 
-var server = app.listen(3000, function() { 
+var server = app.listen(process.env.PORT || 3000, function() { 
     console.log('listining to port 3000') 
 });
